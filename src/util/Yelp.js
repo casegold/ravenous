0669b452786be7050db8c7fec8fbc1498af88569
Casey Goldman <caseygoldman@Caseys-MacBook-Pro.local>
2018-05-01 13:49:26 -0700

    Fetch request Yelp api

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,29 @@
 const apiKey = process.env.YELP_API_KEY;
+
+export const Yelp = {
+  search(term, location, sortBy){
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1 + {business.location.address2 ? ', ' + business.location.address2 : ''},
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories.title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          };
+        });
+      }
+    });
+};
