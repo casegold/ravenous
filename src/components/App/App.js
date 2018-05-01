c18ed7ac6f1aea1972020fb2f46b81c53f442204
Casey Goldman <casegoldman@gmail.com>
2018-05-01 14:38:03 -0700

    Typo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       businesses: []
     };
 
-    this.seachYelp = this.searchYelp.bind(this);
+    this.searchYelp = this.searchYelp.bind(this);
   }
 
   searchYelp(term, location, sortBy) {
